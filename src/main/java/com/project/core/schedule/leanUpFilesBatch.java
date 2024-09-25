a1da1c2c81f9768e41299a882c1926270231b51d
gaegulgaegul <dla9721@gmail.com>
2024-09-25 23:51:16 +0900

    서버 파일 삭제 기능 스케쥴링 추가

---
+++ src/main/java/com/project/core/schedule/leanUpFilesBatch.java
@@ -0,0 +1,22 @@
+package com.project.core.schedule;
+
+import java.time.LocalDate;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.project.application.file.usecase.CleanUpDeactivatedFiles;
+import com.project.core.support.annotation.ScheduledBatch;
+
+import lombok.RequiredArgsConstructor;
+
+@ScheduledBatch
+@RequiredArgsConstructor
+public class leanUpFilesBatch {
+	private final CleanUpDeactivatedFiles cleanUpDeactivatedFiles;
+
+	@Scheduled(cron = "0 0 0 * * ?")
+	public void run() {
+		cleanUpDeactivatedFiles.clean(LocalDate.now());
+	}
+
+}
