84233032b8f3b4be06172188d8aaa4ae709f622b
gaegulgaegul <dla9721@gmail.com>
2024-09-25 15:16:00 +0900

    레디스 포트 중복 사용 케이스 처리

--- src/main/java/com/project/core/config/redis/EmbeddedRedisConfig.java
+++ src/main/java/com/project/core/config/redis/EmbeddedRedisConfig.java
@@ -1,9 +1,14 @@
 package com.project.core.config.redis;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import com.project.core.exception.ApplicationException;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
@@ -24,7 +29,13 @@ public class EmbeddedRedisConfig {
 		redisServer = RedisServer.newRedisServer()
 			.port(redisPort)
 			.build();
+
 		try {
+			boolean useRedisPort = isUseRedisPort();
+			log.info("is alive redis: {}", useRedisPort);
+			if (useRedisPort) {
+				killRedisPort();
+			}
 			redisServer.start();
 			log.info("Redis server started on port {}", redisPort);
 		} catch (Exception e) {
@@ -37,5 +48,41 @@ public class EmbeddedRedisConfig {
 		if (redisServer != null) {
 			redisServer.stop();
 		}
+	}
+
+	private void killRedisPort() {
+		try {
+			String command = "lsof -t -i:%d | xargs kill -9".formatted(redisPort);
+			ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", command);
+			processBuilder.start().waitFor();
+			log.info("Kill Redis port successfully");
+		} catch (Exception e) {
+			log.error("Kill Redis port failed");
+		}
+	}
+
+	private boolean isUseRedisPort() throws IOException {
+		return isRunning(executeGrepProcessCommand(redisPort));
+	}
+
+	private Process executeGrepProcessCommand(int redisPort) throws IOException {
+		String command = String.format("netstat -nat | grep LISTEN|grep %d", redisPort);
+		String[] shell = {"/bin/sh", "-c", command};
+
+		return Runtime.getRuntime().exec(shell);
+	}
+
+	private boolean isRunning(Process process) {
+		String line;
+		StringBuilder pidInfo = new StringBuilder();
+
+		try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+			while ((line = input.readLine()) != null) {
+				pidInfo.append(line);
+			}
+		} catch (Exception e) {
+			throw new ApplicationException();
+		}
+		return StringUtils.hasText(pidInfo.toString());
 	}
 }
